9e3f218bd875cedde191d3a7d7c4e545d983ced0
revati310 <144259898+revati310@users.noreply.github.com>
2024-01-28 18:52:42 -0500

    Update app.js with new msg

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("hi this is from container" + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
